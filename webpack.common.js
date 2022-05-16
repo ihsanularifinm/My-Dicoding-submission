db9aab0007a677358e3c363a18658e7c1a327ffc
Muhammad Ihsanul 'Arifin <72428799+ihsanularifinm@users.noreply.github.com>
2022-05-16 12:10:18 +0700

    My Submission

--- webpack.common.js
+++ webpack.common.js
@@ -3,38 +3,39 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/templates/index.html'),
-      filename: 'index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
-        },
-      ],
-    }),
-  ],
+	entry: path.resolve(__dirname, 'src/scripts/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: 'file-loader',
+				options: {
+					name: 'images/[folder]/[name].[ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			favicon: './src/public/images/favicon/map-pin-pointer-with-cafe-or-restaurant-sign-icon-vector-17767172.png',
+			template: path.resolve(__dirname, 'src/templates/index.html'),
+			filename: 'index.html',
+		}),
+		// new CopyWebpackPlugin({
+		// 	patterns: [
+		// 		{
+		// 			from: path.resolve(__dirname, 'src/public/'),
+		// 			to: path.resolve(__dirname, 'dist/'),
+		// 		},
+		// 	],
+		// }),
+	],
 };
